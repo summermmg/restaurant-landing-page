aef415baa3343aae2c4d52a8f7ee5e84a2df4f82
“Summer” <summerzoummg@gmail.com>
2021-03-25 19:21:18 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+//Show menu
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    nav = document.getElementById(navId);
+    
+    if(toggle && nav){
+        toggle.addEventListener('click', () => {
+          
+            nav.classList.toggle('show-menu');
+        });
+    }
+}
+showMenu('nav-toggle','nav-menu');
+
+const navLink = document.querySelectorAll('.nav-link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(el => el.addEventListener('click', linkAction));
